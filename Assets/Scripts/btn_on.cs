d1640978721afa3246a021ebe96c3d8a16618b82
Sdavi3489 <sdavi3489@gmail.com>
2023-05-24 21:22:07 -0300

    Primeiro Commit: adicionando sistema solar com animação dos planetas em órbita e prévia dos botões virtuais

---
+++ Assets/Scripts/btn_on.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class btn_on : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private GameObject vbButtonObject;
+    private GameObject SolarSystem;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbButtonObject = GameObject.Find("btn_on");
+        SolarSystem = GameObject.Find("sistema_solar_3d");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Botão on pressionado");
+        SolarSystem.GetComponent<Animation>().Play();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Botão off");
+        SolarSystem.GetComponent<Animation>().Stop();
+    }
+}
