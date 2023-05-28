9bc4e9923c7a9400334055d4580ada456738e5bb
Sdavi3489 <sdavi3489@gmail.com>
2023-05-28 13:17:21 -0300

    commit2: adicionando script do botao para acionar animacao, e adicionando animacao do sistema solar

--- Assets/Scripts/btn_on.cs
+++ Assets/Scripts/btn_on.cs
@@ -3,26 +3,28 @@ using Vuforia;
 
 public class btn_on : MonoBehaviour, IVirtualButtonEventHandler
 {
-    private GameObject vbButtonObject;
-    private GameObject SolarSystem;
+    public GameObject vbButtonObject;
+    //public GameObject SolarSystem;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         vbButtonObject = GameObject.Find("btn_on");
-        SolarSystem = GameObject.Find("sistema_solar_3d");
+        //SolarSystem = GameObject.Find("sistema_solar_3d");
         vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        animator.GetComponent<Animator>();
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         Debug.Log("Botão on pressionado");
-        SolarSystem.GetComponent<Animation>().Play();
+        animator.Play("none");
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        Debug.Log("Botão off");
-        SolarSystem.GetComponent<Animation>().Stop();
+        Debug.Log("Botão released");
+        animator.Play("Take 001");
+        
     }
 }
